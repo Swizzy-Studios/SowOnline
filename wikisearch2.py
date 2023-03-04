599b1253ad7175fdf7f8e5d67444e371a6f3c251
Swizzy-Studios <113796265+Swizzy-Studios@users.noreply.github.com>
2023-03-04 17:30:06 -0500

    further

--- wikisearch2.py
+++ wikisearch2.py
@@ -4,8 +4,14 @@ import html
 import re
 
 def wikiscrape (inurl):
-    numberwords = ['first','second','third','fourth','fifth','sixth','seventh','eighth','nineth','tenth','eleventh','twelfth','thirteenth','fourteenth','fifteenth','sixteenth','seventeenth','eighteenth','nineteenth','twentieth','thertieth','fourtieth','fiftieth','sixtieth','seventieth','eightieth','ninetieth','hundredth','thousandth','one','two','three','four','five','six','seven','eight','nine','ten']
-    wordnumbers = ['1st','2nd','3rd','4th','5th','6th','7th','8th','9th','10th',
+    numberwords = ['first','second','third','fourth','fifth','sixth','seventh','eighth','nineth','tenth',
+        'eleventh','twelfth','thirteenth','fourteenth','fifteenth','sixteenth','seventeenth','eighteenth','nineteenth',
+        'twentieth','thirtieth','fourtieth','fiftieth','sixtieth','seventieth','eightieth','ninetieth','hundredth','thousandth', 
+        'one','two','three','four','five','six','seven','eight','nine','ten',
+        'twenty','thirty','fourty','fifty','sixty','seventy','eighty','ninety',
+        'hundred','thousand','million','billion','trillion','millionth','billionth','trillionth']
+    wordnumbers = ['1st','2nd','3rd','4th','5th','6th','7th','8th','9th','10th','11th','12th','13th','14th','15th','16th','17th','18th','19th','20th','30th','40th','50th','60th','70th','80th','90th','00th','000th','1','2','3','4','5','6','7','8','9','10','20','30','40','50','60','70','80','90','00','000','000000','000000000','000000000000','000000th','000000000th','000000000000th']
+    
     try:
         page = requests.get(inurl)
     except:
@@ -45,7 +51,63 @@ def wikiscrape (inurl):
                     wfcheck = 1
             if wfcheck == 0:        
                 seeds.append(html.unescape(title))
+                # strsplit = html.unescape(title).split(' ')
+                # spc = ' '
                 
+                # strsplitc = 0
+                # for word in strsplit:
+                    # nconvert = word.split('-')
+                    # jnum = []
+                    # numberword = 1
+                    # for n in nconvert:
+                        # if n.lower() in numberwords:
+                            # i = numberwords.index(n.lower())
+                            # jnum.append(wordnumbers[i])
+                            # #print(nconvert)
+                        # else:
+                            # numberword = 0
+                    # if numberword == 0:
+                        # continue
+                    # x = len(jnum)
+
+                    # nobj = []
+                    # if x > 1:
+                        # c = 0
+                        # nobji = 0
+                        # for nw in jnum:
+                            
+                            # dc = 0
+                            # for d in nw:
+                                # try:
+                                    # nobj[nobji + dc] = d
+                                # except:
+                                    # nobj.append(d)
+                                # dc+=1
+                         
+                            # if c < (x -1):
+                                # nl = len(nw)
+                                
+                                # nlx = len(jnum[c+1].replace('th','').replace('nd','').replace('rd',''))
+                                
+                                # if nl >= nlx:
+                                    # nobji += (nl - nlx)
+                                # else:
+                                    # nobji += nl
+                            # c+=1
+                    # else:
+                        # for d in jnum:
+                            # nobj.append(d)
+                                            
+                                
+                    # s = ''
+                    # s = s.join(nobj)
+                    # #print(s)
+                    # strsplit[strsplitc] = s
+                    # strsplitc+=1
+                # seeds.append(spc.join(strsplit))
+                
+
+
             if title != presentation:    
                 for wf in wfilter:
                     if wf.lower() in presentation.lower():
